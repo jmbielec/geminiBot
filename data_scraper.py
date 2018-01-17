d1494408b1d0db3c4b204077ac208c70d278d2e0
jmbielec <jmbielec@terpmail.umd.edu>
2018-01-17 00:36:13 -0500

    added documentation for data_scraper.py and functions to convert between human readable and epoch timestamps, and back

--- data_scraper.py
+++ data_scraper.py
@@ -5,13 +5,6 @@ import sqlite3
 import sys
 from sqlite3 import Error
 
-# TODO collect_transactions() should take in a 'time' argument and should parse the transactions from that point,
-# TODO (cont.) returning a list of transactions tuples in the proper format.
-
-# TODO add method that takes a specified 'time' and runs collect_transactions on it, then finds where the transactions
-# TODO (cont.) ended and calls collect_transactions on the last timestamp.  continues until total transactions returned
-# TODO (cont.) is less than 500
-
 
 def data_scraper():
     """Scrapes data from Gemini.
@@ -34,6 +27,7 @@ def data_scraper():
 
 
 def create_connection(database):
+    """Establishes a connection with the given sqlite database."""
     try:
         conn = sqlite3.connect(database)
         return conn
@@ -42,16 +36,39 @@ def create_connection(database):
         sys.exit(1)
 
 
-def collect_transactions():
+def collect_transactions(timestamp):
+    """Collects Gemini trade transactions.
+
+    This function returns a list of all trades/transactions made on Gemini for BTC/USD after the given timestamp
+    up through the time when the function is called.
+
+    :param timestamp: human readable timestamp (e.g. '2018-1-12 00:00:00')
+    :return: returns the list of transactions in the form of tuples
+            (eg. [(timestamp, timestampms, tid, price_usd, amount_btc, transaction_type)])
+
+    """
+
+    start_time = regular_to_epoch(timestamp)
+    current_time = int(time.time())
+
+    # TODO while greater than start_time and less that current_time, keeping adding transactions to transaction list
+    # TODO the last page of transactions could be less than 500 so end after that or after current_time has been reached
+
     transaction_list = [(12, 4, 1032, 9.9, 9.9, "sell"), (932, 4, 103323, 9.9, 9.9, "sell"), (9, 4, 10443, 923.9, 9.329, "sell"), (12, 4, 1032, 9.9, 9.9, "sell"), (932, 4, 103323, 9.9, 9.9, "sell"), (9, 4, 10443, 923.9, 9.329, "sell")]
 
-    # date = int(time.mktime(time.strptime('2018-1-12 00:00:00', '%Y-%m-%d %H:%M:%S')))
     # response = urllib.request.urlopen("https://api.gemini.com/v1/trades/btcusd?timestamp=%s&limit_trades=500" % date)
 
     return transaction_list, len(transaction_list)
 
 
 def insert_transactions(conn, transaction_list):
+    """Inserts rows of transactions to database.
+
+    Inserts the entire list of transactions into the GeminiHistoricalDatabase, with the exception of
+    transactions that already exist in the database as identified by their tid (transaction ID).
+
+    """
+
     sql = ''' INSERT OR IGNORE INTO GeminiTradeData (timestamp, timestampms, tid, price_usd, amount_btc, transaction_type) 
               VALUES (?, ?, ?, ?, ?, ?)'''
 
@@ -59,6 +76,28 @@ def insert_transactions(conn, transaction_list):
     cur.executemany(sql, transaction_list)
 
     conn.commit()
+
+
+def regular_to_epoch(timestamp):
+    """Converts from human readable date to epoch date.
+
+    :param timestamp: human readable timestamp (e.g. '2018-1-12 00:00:00')
+    :return: epoch timestamp (e.g. 1515715200)
+
+    """
+
+    return int(time.mktime(time.strptime(timestamp, '%Y-%m-%d %H:%M:%S'))) - time.timezone
+
+
+def epoch_to_regular(timestamp):
+    """Converts from epoch to human readable date.
+
+    :param timestamp: epoch timestamp (e.g. 1515715200)
+    :return:  human readable timestamp (e.g. '2018-1-12 00:00:00')
+
+    """
+
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(timestamp))
 
 
 # this function is only for testing and development purposes
