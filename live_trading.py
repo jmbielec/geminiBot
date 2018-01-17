b5810e9cda6291d056837228c3e8938a3cb27167
jmbielec <jmbielec@terpmail.umd.edu>
2018-01-16 23:59:14 -0500

    added a way for me to push new code to github without revealing my api keys, or having to remove them everytime I want to push code!

--- live_trading.py
+++ live_trading.py
@@ -5,10 +5,18 @@ import hashlib
 import time
 import json
 
+""" api_keys is a python file consisting solely of variables where I store my api keys so I don't accidentally reveal
+    them to everyone on github. Comment out or delete the following lines of code that contain those keys,
+    then uncomment the two comments following that to include your own api keys. Be careful not to show anyone those
+    keys! If you accidentally reveal them, you can delete your api keys from your Gemini account and get new ones."""
+from api_keys import sandbox_api_secret  # delete this and uncomment the lines below
+from api_keys import sandbox_api_key  # delete this and uncomment the lines below
+
+# sandbox_api_key = 'insert your api public key here'
+# sandbox_api_secret = b'insert your api secret key here'
+
 sandbox_url = 'https://api.sandbox.gemini.com/v1/balances'
 
-sandbox_api_key = 'insert your api key here'
-sandbox_api_secret = b'insert your api key here'
 
 
 nonce = int(time.time() * 1000)
